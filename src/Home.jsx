2079d0f21a0db9c2935e58e05d472daa1956b37d
Gauri Tawale <113706508+gauritawale@users.noreply.github.com>
2025-01-09 08:13:57 +0530

    Add Home Section

--- src/Home.jsx
+++ src/Home.jsx
@@ -3,7 +3,20 @@ import React from "react";
 function Home (){
     return(
         <>
-        <h1>Home Page</h1>
+        <section id="home" style="overflow:hidden">
+    <div class="container-fluid main" style="overflow:hidden">
+        <div class="row mt-3">
+            <div class="col-md-12 mt-5">
+                    <h2 class="h11">Gauri</h2>
+                    <h1 class="h12"><span style={{'text-shadow': '2px 0px 0px rgb(7, 39, 204)'}}>Full Stack Python </span><span style="text-shadow: 2px 0px 0px rgb(15, 3, 82)">Developer</span></h1>
+
+
+                    
+            </div>
+        </div>
+    </div>
+</section>
+
         
         </>
 
